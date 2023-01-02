a314a4de1e36061b52e7d8c7e4a7e6ffd0e6034b
HamzaARS2 <hamza1arrastec@gmail.com>
2023-01-02 17:21:46 +0000

    Category Repository created

---
+++ app/src/main/java/com/reddevx/groceriesapp/data/network/repository/CategoryRepository.kt
@@ -0,0 +1,29 @@
+package com.reddevx.groceriesapp.data.network.repository
+
+import com.reddevx.groceriesapp.api.CategoryService
+import com.reddevx.groceriesapp.model.Category
+import javax.inject.Inject
+
+class CategoryRepository @Inject constructor(
+    private val service: CategoryService
+) : IRepository<Category,Int> {
+    override suspend fun insert(data: Category): Category {
+        return service.insertCategory(data)
+    }
+
+    override suspend fun retrieve(id: Int): Category? {
+        return service.retrieveCategory(id)
+    }
+
+    override suspend fun retrieveAll(): List<Category> {
+        return service.retrieveAllCategories()
+    }
+
+    override suspend fun update(id: Int, data: Category): Category {
+        return service.updateCategory(id,data)
+    }
+
+    override suspend fun delete(id: Int): String {
+        return service.deleteCategory(id)
+    }
+}
